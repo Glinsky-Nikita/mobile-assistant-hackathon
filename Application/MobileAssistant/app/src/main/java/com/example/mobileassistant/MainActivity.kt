75fe66c51bddecb26ba4bfe7b61c9bfbae899322
DaniilKubantsev <106108029+DaniilKubantsev@users.noreply.github.com>
2025-04-05 18:09:09 +0300

    a

--- Application/MobileAssistant/app/src/main/java/com/example/mobileassistant/MainActivity.kt
+++ Application/MobileAssistant/app/src/main/java/com/example/mobileassistant/MainActivity.kt
@@ -12,8 +12,6 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
--- Application/MobileAssistant/app/src/main/java/com/example/mobileassistant/domain/audio_recorder/VoiceRecorder.kt
+++ Application/MobileAssistant/app/src/main/java/com/example/mobileassistant/domain/audio_recorder/VoiceRecorder.kt
@@ -14,6 +14,7 @@ import java.text.SimpleDateFormat
 import java.util.Date
 
 class VoiceRecorder(
+    private val context: Context
 ): AudioRecorder {
     private var recorder: MediaRecorder? = null
 
